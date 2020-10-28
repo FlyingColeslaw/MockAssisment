902964b4b1d07deccd47f0ae99edd33b75f3ebc7
Blake Bruseloff <71026419+FlyingColeslaw@users.noreply.github.com>
2020-10-28 11:17:16 -0400

    Add files via upload

---
+++ MockAssessment.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace MockAssessment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+
+
+            int a = FindBuildingType(5);
+
+            IsTheSame(5, 6);
+            IsTheSame(5, 5);
+            IsTheSame(0, 8);
+
+
+            double num1 = Subtract(4, 6);
+            double num2 = Subtract(8, 4);
+        }
+
+
+        private static bool IsTheSame(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+        static double Subtract(double num1, double num2)
+        {
+
+            double total = num2 - num1;
+            return total;
+
+        }
+
+        static int FindBuildingType(int a)
+        {
+
+            if (a > 3 && a < 11)
+            {
+                Console.WriteLine("This is an office building");
+            }
+
+           else if (a > 49)
+            {
+                Console.WriteLine("This is a SUPER skyscraper!");
+            }
+
+
+            else if (a > 10 && a < 50)
+            {
+                Console.WriteLine("This is a SUPER skyscraper!");
+            }
+
+            else if (a < 4)
+            {
+                Console.WriteLine("This is a house!!");
+            }
+
+            return a;
+
+        }
+
+    }
+
+}
+
+
+
+    
